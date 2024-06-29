cacf1325b7c8fbf9b88544592b606dfa7e2866bd
lucas castro <lucas_o__castro@hotmail.com>
2024-06-29 03:10:51 -0300

    Basic Entity.cs

---
+++ ECSLib/Entity.cs
@@ -0,0 +1,16 @@
+﻿namespace ECSLib;
+
+public struct Entity: IEquatable<Entity>
+{
+    public int ID { get; }
+    
+    public bool Equals(Entity other) => ID == other.ID;
+
+    public override bool Equals(object? obj) => obj is Entity other && Equals(other);
+
+    public override int GetHashCode() => ID;
+
+    public static bool operator ==(Entity left, Entity right) => left.Equals(right);
+
+    public static bool operator !=(Entity left, Entity right) => !(left == right);
+}
