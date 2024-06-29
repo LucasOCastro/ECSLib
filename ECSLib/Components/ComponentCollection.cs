3d3bc7e13078ebe32dde5c1992f11c6b0a0081bf
lucas castro <lucas_o__castro@hotmail.com>
2024-06-29 04:32:05 -0300

    Fixes ComponentCollection.cs to avoid boxing in ComponentManager.cs

--- ECSLib/Components/ComponentCollection.cs
+++ ECSLib/Components/ComponentCollection.cs
@@ -1,41 +1,56 @@
-﻿using System.Collections;
+﻿using System.Runtime.InteropServices;
 using ECSLib.Exceptions;
 
-namespace ECSLib;
+namespace ECSLib.Components;
 
-internal class ComponentCollection<TComponent> : IEnumerable<TComponent> where TComponent : struct
+
+internal class ComponentCollection
 {
     private const int ArrayLengthIncrement = 100;
     
     public int Count { get; private set; }
     
-    private TComponent[] _components = new TComponent[ArrayLengthIncrement];
+    private byte[] _components = new byte[ArrayLengthIncrement];
     private readonly Dictionary<int, int> _entityIdToCompIndex = new();
     private readonly Dictionary<int, int> _compIndexToEntityId = new();
+
+    private readonly Type _componentType;
+    private readonly int _componentSize;
+    public ComponentCollection(Type componentType)
+    {
+        _componentType = componentType;
+        _componentSize = Marshal.SizeOf(_componentType);
+    }
+
+    private Span<TComponent> GetSpanAt<TComponent>(int index) where TComponent : struct
+    {
+        Span<byte> span = new(_components, index, _componentSize);
+        return MemoryMarshal.Cast<byte, TComponent>(span);
+    }
     
     /// <returns>
-    /// A reference to the component. Do not store this reference, or it will
-    /// be outdated when the collection updates.
+    /// A reference to the component.
+    /// Do not store this reference, or it will be outdated when the collection updates.
     /// </returns>
     /// <exception cref="MissingComponentException">Thrown if the entity does not have the component.</exception>
-    public ref TComponent Get(Entity entity)
+    public ref TComponent Get<TComponent>(Entity entity) where TComponent : struct
     {
         if (!_entityIdToCompIndex.TryGetValue(entity.ID, out int index))
         {
-            throw new MissingComponentException(typeof(TComponent), entity);
+            throw new MissingComponentException(_componentType, entity);
         }
-        return ref _components[index];
+        return ref GetSpanAt<TComponent>(index)[0];
     }
 
     /// <summary>
     /// Registers a component associated with the entity.
     /// </summary>
     /// <exception cref="DuplicatedComponentException">Thrown if the entity already has the component.</exception>
-    public void Register(Entity entity, TComponent component)
+    public void Register<TComponent>(Entity entity, TComponent component) where TComponent : struct
     {
         if (_entityIdToCompIndex.ContainsKey(entity.ID))
         {
-            throw new DuplicatedComponentException(typeof(TComponent), entity);
+            throw new DuplicatedComponentException(_componentType, entity);
         }
 
         if (Count == _components.Length)
@@ -43,9 +58,10 @@ internal class ComponentCollection<TComponent> : IEnumerable<TComponent> where T
             Expand();
         }
 
+        // Adds the component after the last component in the array.
         _entityIdToCompIndex.Add(entity.ID, Count);
         _compIndexToEntityId.Add(Count, entity.ID);
-        _components[Count] = component;
+        GetSpanAt<TComponent>(Count).Fill(component);
         Count++;
     }
 
@@ -53,7 +69,7 @@ internal class ComponentCollection<TComponent> : IEnumerable<TComponent> where T
     /// Removes the component associated with the entity..
     /// </summary>
     /// <exception cref="MissingComponentException">Thrown if the entity does not have the component.</exception>
-    public void Unregister(Entity entity)
+    public void Unregister<TComponent>(Entity entity) where TComponent : struct
     {
         if (!_entityIdToCompIndex.TryGetValue(entity.ID, out int compIndex))
         {
@@ -62,20 +78,20 @@ internal class ComponentCollection<TComponent> : IEnumerable<TComponent> where T
         
         _entityIdToCompIndex.Remove(entity.ID);
         _compIndexToEntityId.Remove(compIndex);
-        FillPosition(compIndex);
+        FillPosition<TComponent>(compIndex);
         Count--;
     }
 
     /// <summary>
     /// Moves an element from the end of the component array to the empty index, ensuring it is packed.
     /// </summary>
-    private void FillPosition(int emptyCompIndex)
+    private void FillPosition<TComponent>(int emptyCompIndex) where TComponent : struct
     {
         // If the empty position is at the end of the array, we do not need to fill it up, just clear that specific position.
         int lastCompIndex = Count - 1;
         if (emptyCompIndex == lastCompIndex)
         {
-            _components[emptyCompIndex] = default;
+            GetSpanAt<TComponent>(emptyCompIndex).Clear();
             return;
         }
         
@@ -86,9 +102,9 @@ internal class ComponentCollection<TComponent> : IEnumerable<TComponent> where T
         int lastEntityId = _compIndexToEntityId[Count - 1];
         _entityIdToCompIndex[lastEntityId] = emptyCompIndex;
 
-        // Actually perform the update and clear the posiiton which was now left empty.
-        _components[emptyCompIndex] = _components[lastCompIndex];
-        _components[lastCompIndex] = default;
+        // Actually perform the update and clear the position which was now left empty.
+        GetSpanAt<TComponent>(lastCompIndex).CopyTo(GetSpanAt<TComponent>(emptyCompIndex));
+        GetSpanAt<TComponent>(lastCompIndex).Clear();
     }
 
     /// <summary>
@@ -96,9 +112,6 @@ internal class ComponentCollection<TComponent> : IEnumerable<TComponent> where T
     /// </summary>
     private void Expand()
     {
-        Array.Resize(ref _components, Count + ArrayLengthIncrement);
+        Array.Resize(ref _components, _componentSize * (Count + ArrayLengthIncrement));
     }
-
-    public IEnumerator<TComponent> GetEnumerator() => _components.AsEnumerable().GetEnumerator();
-    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 }
---
+++ ECSLib/Components/ComponentManager.cs
@@ -0,0 +1,43 @@
+﻿namespace ECSLib.Components;
+
+public class ComponentManager
+{
+    private readonly Dictionary<Type, ComponentCollection> _collections = new();
+
+    private ComponentCollection GetCollection(Type type)
+    {
+        if (!_collections.TryGetValue(type, out var collection))
+        {
+            collection = new(type);
+            _collections.Add(type, collection);
+        }
+        return collection;
+    }
+
+    private ComponentCollection GetCollection<TComponent>() => GetCollection(typeof(TComponent));
+
+    /// <inheritdoc cref="ComponentCollection.Get{TComponent}"/>
+    public ref TComponent GetComponent<TComponent>(Entity entity) where TComponent : struct =>
+        ref GetCollection<TComponent>().Get<TComponent>(entity);
+
+    /// <summary>
+    /// Adds a component to an Entity using its default constructor.
+    /// </summary>
+    /// <inheritdoc cref="ComponentCollection.Register{TComponent}"/>
+    public void AddComponent<TComponent>(Entity entity) where TComponent : struct =>
+        GetCollection<TComponent>().Register(entity, new TComponent());
+    
+    /// <summary>
+    /// Adds a component to an Entity.
+    /// </summary>
+    /// <inheritdoc cref="ComponentCollection.Register{TComponent}"/>
+    public void AddComponent<TComponent>(Entity entity, TComponent component) where TComponent : struct =>
+        GetCollection<TComponent>().Register(entity, component);
+
+    /// <summary>
+    /// Removes a component from an entity. 
+    /// </summary>
+    /// <inheritdoc cref="ComponentCollection.Unregister{TComponent}"/>
+    public void RemoveComponent<TComponent>(Entity entity) where TComponent : struct =>
+        GetCollection<TComponent>().Unregister<TComponent>(entity);
+}
