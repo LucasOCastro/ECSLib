284f7e24d8268f63c3da3ccd639a2b5a6eedee8e
lucas castro <lucas_o__castro@hotmail.com>
2024-06-30 04:27:17 -0300

    More collection extension

---
+++ ECSLib/Extensions/CollectionExtension.cs
@@ -0,0 +1,7 @@
+﻿namespace ECSLib.Extensions;
+
+public static class CollectionExtension
+{
+    public static IEnumerable<T> Except<T>(this IEnumerable<T> col, T val) =>
+        col.Where(obj => val != null && !val.Equals(obj));
+}
