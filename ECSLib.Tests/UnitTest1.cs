62b7fe0438b8612dd879687f559dc51524b3b343
lucas castro <lucas_o__castro@hotmail.com>
2024-06-30 06:22:12 -0300

    Creates unit tests project

---
+++ ECSLib.Tests/UnitTest1.cs
@@ -0,0 +1,130 @@
+using System.Reflection;
+using ECSLib.Archetypes;
+using ECSLib.Components.Exceptions;
+
+namespace ECSLib.Tests;
+
+public struct TestComponentA
+{
+    public int Data;
+}
+
+public struct TestComponentB
+{
+    public bool BoolData;
+}
+
+public class Tests
+{
+    private ECS _world;
+    
+    [SetUp]
+    public void Setup()
+    {
+        _world = new();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+    }
+    
+    [Test, Order(0)]
+    public void TestEntityCreateDestroy()
+    {
+        //Creates an entity and asserts it's the first entity.
+        var entityA = _world.CreateEntity();
+        Assert.That(entityA.ID, Is.Zero);
+        
+        //Creates another entity and asserts it's the next.
+        //Creates an entity and asserts it's the first entity.
+        var entityB = _world.CreateEntity();
+        Assert.That(entityB.ID, Is.EqualTo(1));
+        
+        //Destroys the first entity and assert a new one will have index 0.
+        _world.DestroyEntity(entityA);
+        var entityC = _world.CreateEntity();
+        Assert.That(entityC.ID, Is.Zero);
+        
+        //Assert a new entity will have index 2
+        var entityD = _world.CreateEntity();
+        Assert.That(entityD.ID, Is.EqualTo(2));
+    }
+    
+    [Test, Order(1)]
+    public void TestEntityComponentAddRemove()
+    {
+        const int testValueSet = 15;
+        const int testValueInit = 20;
+        
+        var entity = _world.CreateEntity();
+        
+        //Adds a component and assert it is the default value.
+        _world.AddComponent<TestComponentA>(entity);
+        Assert.That(_world.GetComponent<TestComponentA>(entity), Is.EqualTo(default(TestComponentA)));
+        
+        //Asserts it's not possible to add a component of the same type.
+        Assert.Throws<DuplicatedComponentException>(() => _world.AddComponent<TestComponentA>(entity));
+        
+        //Change a component's data and assert the value has changed; 
+        ref var refComp = ref _world.GetComponent<TestComponentA>(entity);
+        refComp.Data = testValueSet;
+        Assert.That(_world.GetComponent<TestComponentA>(entity).Data, Is.EqualTo(testValueSet));
+        
+        //Remove a component and assert it doesn't exist anymore.
+        _world.RemoveComponent<TestComponentA>(entity);
+        Assert.Throws<MissingComponentException>(() => _world.GetComponent<TestComponentA>(entity));
+        
+        //Add a component with a predefined value and assert it is the initiated value.
+        _world.AddComponent(entity, new TestComponentA{Data = testValueInit});
+        Assert.That(_world.GetComponent<TestComponentA>(entity).Data, Is.EqualTo(testValueInit));
+    }
+
+    [Test, Order(2)]
+    public void TestEntityArchetypeUpdate()
+    {
+        var managerField = typeof(ECS)
+            .GetFields(BindingFlags.Instance | BindingFlags.NonPublic)
+            .First(f => f.FieldType == typeof(ArchetypeManager));
+        var archetypeManager = managerField.GetValue(_world) as ArchetypeManager;
+        Assert.That(archetypeManager, Is.Not.Null);
+
+        var storageField = typeof(ArchetypeManager)
+            .GetFields(BindingFlags.Instance | BindingFlags.NonPublic)
+            .First(f => f.FieldType == typeof(ArchetypeStorage));
+        var archetypeStorage = storageField.GetValue(archetypeManager) as ArchetypeStorage;
+        Assert.That(archetypeStorage, Is.Not.Null);
+     
+        //Creates an entity and asserts its archetype is empty
+        var entityA = _world.CreateEntity();
+        Assert.That(!archetypeManager.GetAllComponentTypes(entityA).Any());
+        
+        //Adds a component and assert its archetype is the component
+        _world.AddComponent<TestComponentA>(entityA);
+        Assert.That(archetypeManager.GetAllComponentTypes(entityA).SequenceEqual(new []{typeof(TestComponentA)}));
+        
+        //Adds another component and assert its archetype is both components
+        _world.AddComponent<TestComponentB>(entityA);
+        Assert.That(archetypeManager.GetAllComponentTypes(entityA).SequenceEqual(new []{typeof(TestComponentA), typeof(TestComponentB)}));
+        
+        //Creates another entity and assert their archetypes are different
+        var entityB = _world.CreateEntity();
+        Assert.That(archetypeStorage.GetOrCreateArchetype(archetypeManager.GetAllComponentTypes(entityA)),
+            Is.Not.EqualTo(archetypeStorage.GetOrCreateArchetype(archetypeManager.GetAllComponentTypes(entityB))));
+        
+        //Adds the components to the entity and assert their archetypes are equal (ordered insertion)
+        _world.AddComponent<TestComponentA>(entityB);
+        _world.AddComponent<TestComponentB>(entityB);
+        Assert.That(archetypeStorage.GetOrCreateArchetype(archetypeManager.GetAllComponentTypes(entityA)),
+            Is.EqualTo(archetypeStorage.GetOrCreateArchetype(archetypeManager.GetAllComponentTypes(entityB))));
+        
+        //Adds the components to the entity and assert their archetypes are equal (unordered insertion)
+        _world.RemoveComponent<TestComponentA>(entityB);
+        _world.AddComponent<TestComponentA>(entityB);
+        Assert.That(archetypeStorage.GetOrCreateArchetype(archetypeManager.GetAllComponentTypes(entityA)),
+            Is.EqualTo(archetypeStorage.GetOrCreateArchetype(archetypeManager.GetAllComponentTypes(entityB))));
+    }
+    
+    
+    
+}
