ce2058ddd97d061feb8dd9bc430420487ddd196e
lucas castro <lucas_o__castro@hotmail.com>
2024-06-30 03:59:31 -0300

    EntityManager to managing entity instancing

---
+++ ECSLib/Entities/EntityManager.cs
@@ -0,0 +1,25 @@
+﻿namespace ECSLib.Entities;
+
+public class EntityManager
+{
+    /// <summary>
+    /// Stores the ids which have been freed on entity deletion and should be used next.
+    /// </summary>
+    private readonly Stack<int> _freedIdStack = new();
+    private int _entityCount;
+
+    private int GetNextId() => _freedIdStack.TryPop(out int index) ? index : _entityCount;
+    
+    public Entity CreateEntity()
+    {
+        int id = GetNextId();
+        _entityCount++;
+        return new(id);
+    }
+    
+    public void RemoveEntity(Entity entity)
+    {
+        _freedIdStack.Push(entity.ID);
+        _entityCount--;
+    }
+}
