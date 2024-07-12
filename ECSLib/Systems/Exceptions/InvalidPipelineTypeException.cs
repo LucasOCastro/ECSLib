4c972720ca3447b0f84ff690155593875e15f137
lucas castro <lucas_o__castro@hotmail.com>
2024-07-12 20:56:15 -0300

    Attributes and exceptions related to pipelines

---
+++ ECSLib/Systems/Exceptions/InvalidPipelineTypeException.cs
@@ -0,0 +1,4 @@
+﻿namespace ECSLib.Systems.Exceptions;
+
+internal class InvalidPipelineTypeException(Type pipelineEnumType)
+    : Exception($"{pipelineEnumType.FullName} is not a valid enum type.");
---
+++ ECSLib/Systems/Exceptions/MissingPipelineEnumTypeException.cs
@@ -0,0 +1,6 @@
+﻿using ECSLib.Systems.Attributes;
+
+namespace ECSLib.Systems.Exceptions;
+
+internal class MissingPipelineEnumTypeException()
+    : Exception($"Tried to register a pipeline enum type but no enum was flagged with {nameof(PipelineEnumAttribute)}.");
