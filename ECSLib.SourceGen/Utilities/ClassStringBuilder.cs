53e76b9e69167bae607e05763288eb031f54c09f
lucas castro <lucas_o__castro@hotmail.com>
2024-07-12 16:24:06 -0300

    More functions and constructors for ClassStringBuilder.cs

--- ECSLib.SourceGen/Utilities/ClassStringBuilder.cs
+++ ECSLib.SourceGen/Utilities/ClassStringBuilder.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace ECSLib.SourceGen.Utilities;
@@ -9,23 +10,42 @@ namespace ECSLib.SourceGen.Utilities;
 /// </summary>
 public class ClassStringBuilder
 {
+    public readonly struct GenericArgDef(string arg, string[] constraints)
+    {
+        public readonly string Arg = arg;
+        public readonly string[] Constraints = constraints;
+        
+        public static implicit operator GenericArgDef(string arg) => new(arg, []);
+    }
+    
     private readonly StringBuilder _builder = new();
     
     private const string Tab = "    ";
     private int _level;
     private readonly StringBuilder _indent = new();
 
-    public ClassStringBuilder(ICollection<string> namespaces, string className)
+    public ClassStringBuilder()
+    {
+    }
+    
+    public ClassStringBuilder(string namespaceName)
+    {
+        OpenNamespace(namespaceName);
+    }
+
+    public ClassStringBuilder(string namespaceName, string className)
+    {
+        OpenNamespace(namespaceName);
+        OpenPartialClass(className);
+    }
+    
+    public ClassStringBuilder(IEnumerable<string> namespaces, string className)
     {
         foreach (var name in namespaces)
         {
-            PushLine("namespace ");
-            _builder.Append(name);
-            Open();
+            OpenNamespace(name);
         }
-        PushLine("partial class ");
-        _builder.Append(className);
-        Open();
+        OpenPartialClass(className);
     }
 
     /// <summary> Finishes the class, closing every open block and returning the string. </summary>
@@ -38,7 +58,7 @@ public class ClassStringBuilder
 
         return _builder.ToString();
     }
-
+    
     /// <summary> Adds a correctly indented line. </summary>
     public void PushLine(string line)
     {
@@ -47,7 +67,7 @@ public class ClassStringBuilder
     }
 
     /// <summary> If in a line, break first. Then initializes the current line with correct indentation. </summary>
-    private void InitLine()
+    public void InitLine()
     {
         _builder.AppendLine();
         _builder.Append(_indent);
@@ -130,6 +150,57 @@ public class ClassStringBuilder
         else _builder.Append('>');
     }
     
+    /// <summary>
+    /// Appends a full method signature.<br/>
+    /// '<see cref="signature"/>' '<see cref="methodName"/>' ('<see cref="args"/>')
+    /// </summary>
+    /// <example>
+    /// signature = "public partial void"; methodName = "Foo", args = ["string Bar"]<br/>
+    /// <b>Result:</b><br/>
+    /// public partial void Foo(string Bar)
+    /// </example>
+    public void PushMethodSignature(string signature, string methodName, params string[] args)
+    {
+        _builder.Append(signature);
+        _builder.Append(' ');
+        _builder.Append(methodName);
+        PushArgumentList(args);
+    }
+    
+    /// <summary>
+    /// Appends a full generic method signature.<br/>
+    /// '<see cref="signature"/>' '<see cref="methodName"/>'&lt;T1, T2, ...&gt;('<see cref="args"/>') where T1 : x where T2 : y 
+    /// </summary>
+    /// <example>
+    /// signature = "public partial void"; methodName = "Foo", genericArgs = [{"T1", "struct"}] args = ["string Bar"]<br/>
+    /// <b>Result:</b><br/>
+    /// public partial void Foo&lt;T1&gt;(string Bar) where T1: struct
+    /// </example>
+    public void PushGenericMethodSignature(string signature, string methodName, ICollection<GenericArgDef> genericArgs, params string[] args)
+    {
+        _builder.Append(signature);
+        _builder.Append(' ');
+        _builder.Append(methodName);
+        PushGenericArgumentList(genericArgs.Select(a => a.Arg));
+        PushArgumentList(args);
+        
+        foreach (var genericArg in genericArgs)
+        {
+            _builder.Append(" where ");
+            _builder.Append(genericArg.Arg);
+            _builder.Append(" : ");
+
+            if (genericArg.Constraints.Length == 0) continue;
+            bool first = true;
+            foreach (var constraint in genericArg.Constraints)
+            {
+                if (!first) _builder.Append(',');
+                first = false;
+                _builder.Append(constraint);
+            }
+        }
+    }
+    
     /// <summary> Increases the indentation level, updating the indentation string properly. </summary>
     private void IncreaseLevel()
     {
@@ -153,7 +224,29 @@ public class ClassStringBuilder
         _builder.Append('{');
         IncreaseLevel();
     }
+    
+    /// <summary>
+    /// Appends a namespace and opens a new block.<br/>
+    /// namespace '<see cref="name"/>'<br/>{
+    /// </summary>
+    public void OpenNamespace(string name)
+    {
+        PushLine("namespace ");
+        _builder.Append(name);
+        Open();
+    }
 
+    /// <summary>
+    /// Appends a partial class signature and opens a new block.<br/>
+    /// partial class '<see cref="name"/>'<br/>{
+    /// </summary>
+    public void OpenPartialClass(string name)
+    {
+        PushLine("partial class ");
+        _builder.Append(name);
+        Open();   
+    }
+    
     /// <summary>
     /// Appends a full method signature and opens a new block.<br/>
     /// '<see cref="signature"/>' '<see cref="methodName"/>' ('<see cref="args"/>')<br/>{
@@ -166,10 +259,23 @@ public class ClassStringBuilder
     public void OpenMethod(string signature, string methodName, params string[] args)
     {
         InitLine();
-        _builder.Append(signature);
-        _builder.Append(' ');
-        _builder.Append(methodName);
-        PushArgumentList(args);
+        PushMethodSignature(signature, methodName, args);
+        Open();
+    }
+
+    /// <summary>
+    /// Appends a full generic method signature and opens a new block.<br/>
+    /// '<see cref="signature"/>' '<see cref="methodName"/>'&lt;T1, T2, ...&gt;('<see cref="args"/>') where T1 : x where T2 : y <br/>{ 
+    /// </summary>
+    /// <example>
+    /// signature = "public partial void"; methodName = "Foo", genericArgs = [{"T1", "struct"}] args = ["string Bar"]<br/>
+    /// <b>Result:</b><br/>
+    /// public partial void Foo&lt;T1&gt;(string Bar) where T1: struct<br/>{
+    /// </example>
+    public void OpenGenericMethod(string signature, string methodName, ICollection<GenericArgDef> genericArgs, params string[] args)
+    {
+        InitLine();
+        PushGenericMethodSignature(signature, methodName, genericArgs, args);
         Open();
     }
 
@@ -181,5 +287,28 @@ public class ClassStringBuilder
         DecreaseLevel();
         InitLine();
         _builder.Append('}');
+    }
+    
+    /// <summary>
+    /// Initializes a builder with the namespaces and nested classes specified.
+    /// Does not support arity for generic classes.
+    /// </summary>
+    public static ClassStringBuilder FromFullMetadataName(string metadataName)
+    {
+        ClassStringBuilder builder = new();
+        int lastNamespaceDot = metadataName.LastIndexOf('.');
+        if (lastNamespaceDot >= 0)
+        {
+            foreach (var ns in metadataName.Substring(0, lastNamespaceDot).Split('.'))
+                if (ns.Length > 0)
+                    builder.OpenNamespace(ns);
+            metadataName = metadataName.Substring(lastNamespaceDot + 1);
+        }
+
+        foreach (var className in metadataName.Split('+'))
+            if (className.Length > 0)
+                builder.OpenPartialClass(className);
+        
+        return builder;
     }
 }
