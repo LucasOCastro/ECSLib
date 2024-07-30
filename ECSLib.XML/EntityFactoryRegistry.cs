8aacdf6995618addfc2a2b3b071c0ac4d455174d
lucas castro <lucas_o__castro@hotmail.com>
2024-07-29 21:58:50 -0300

    Registry to store factories

---
+++ ECSLib.XML/EntityFactoryRegistry.cs
@@ -0,0 +1,43 @@
+﻿using System.Xml;
+using ECSLib.Entities;
+using ECSLib.XML.Exceptions;
+
+namespace ECSLib.XML;
+
+public static class EntityFactoryRegistry
+{
+    private static readonly Dictionary<string, EntityFactoryDelegate> Factories = new();
+    
+    public static void Load(string name, EntityFactoryDelegate factory)
+    {
+        if (!Factories.TryAdd(name, factory))
+        {
+            throw new RepeatedEntityFactoryNameException(name);
+        }
+    }
+
+    public static void LoadFrom(XmlDocument document) =>
+        Load(document.Name, EntityDeserializer.CreateEntityFactory(document));
+
+    public static void LoadAllInDirectory(string dir, bool includeSubdirectories)
+    {
+        foreach (var file in Directory.EnumerateFiles(dir, "*.xml"))
+        {
+            XmlDocument doc = new();
+            doc.Load(file);
+            LoadFrom(doc);
+        }
+
+        if (includeSubdirectories)
+            foreach (var sub in Directory.EnumerateDirectories(dir))
+                LoadAllInDirectory(sub, includeSubdirectories);
+    }
+
+    public static EntityFactoryDelegate GetFactory(string name) => Factories[name];
+    
+    public static Entity CreateNew(string name, ECS world) => Factories[name](world);
+
+    public static void Clear() => Factories.Clear();
+
+    public static void Remove(string name) => Factories.Remove(name);
+}
---
+++ ECSLib.XML/Exceptions/RepeatedEntityFactoryNameException.cs
@@ -0,0 +1,4 @@
+﻿namespace ECSLib.XML.Exceptions;
+
+public class RepeatedEntityFactoryNameException(string name) 
+    : Exception($"Entity Factory was already registered for the name {name}.");
