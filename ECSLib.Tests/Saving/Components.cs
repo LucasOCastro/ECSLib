df9cd184d999d966abb747558c153d54e522a8a0
lucas castro <lucas_o__castro@hotmail.com>
2024-08-27 13:56:30 -0300

    Saving unit tests

---
+++ ECSLib.Tests/Saving/Components.cs
@@ -0,0 +1,23 @@
+﻿using ECSLib.Components.Interning;
+
+namespace ECSLib.Tests.Saving;
+
+public struct CompA
+{
+    public int Value = 10;
+    public PooledRef<List<OneClass>> Classes = new([]);
+    
+    public CompA(){}
+}
+
+public struct CompB
+{
+    public PooledRef<string> Text = new("Yo");
+    
+    public CompB(){}
+}
+
+public class OneClass
+{
+    public int Prop { get; set; }
+}
